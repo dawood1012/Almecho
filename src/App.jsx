da6795b07fbfa9c365d540fb3b123cf8e039325f
dawood1012 <dawood1012md@gmail.com>
2026-05-11 17:05:02 +0500

    Fix routing basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Almecho">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
